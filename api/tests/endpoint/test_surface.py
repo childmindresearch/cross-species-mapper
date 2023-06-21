a65c01ac93ed70e032fa61541ad6872083eb3600
Reinder Vos de Wael <reinder.vosdewael@gmail.com>
2023-06-21 09:32:58 -0400

    Change Docker frontend to work on Apple Silicon. (#2)
    The current version does not work on Apple Silicon. I've added emulation to the frontend emulator, as well as fixed an endpoint definition in the API.

--- api/tests/endpoint/test_surface.py
+++ api/tests/endpoint/test_surface.py
@@ -8,7 +8,7 @@ client = testclient.TestClient(main.app)
 
 def test_get_hemispheres() -> None:
     """Test that the hemispheres are returned."""
-    response = client.get("/surface/hemispheres")
+    response = client.get("/surfaces/hemispheres")
 
     assert response.status_code == status.HTTP_200_OK
     assert response.json().keys() == {"fslr_32k_left", "fslr_32k_right"}
