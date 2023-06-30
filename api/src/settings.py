ef9fb3c35587e28b4966cdcb0788a3ef6318b4dd
Reinder Vos de Wael <reinder.vosdewael@childmind.org>
2023-06-30 17:07:40 -0400

    just mypy ignore

--- api/src/settings.py
+++ api/src/settings.py
@@ -8,7 +8,7 @@ import pydantic
 CURRENT_DIR = pathlib.Path(__file__).parent
 
 
-class Settings(pydantic.BaseSettings):
+class Settings(pydantic.BaseSettings):  # mypy: ignore
     """Settings for the API."""
 
     LOGGER_NAME: str = pydantic.Field("Cross Species Mapper API", env="LOGGER_NAME")
