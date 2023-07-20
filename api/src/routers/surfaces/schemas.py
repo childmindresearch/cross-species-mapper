664be36ca029a2e6e99e5f45dd38ddb73cca3144
Reinder Vos de Wael <reinder.vosdewael@childmind.org>
2023-07-20 14:03:21 -0400

    float coordinate

--- api/src/routers/surfaces/schemas.py
+++ api/src/routers/surfaces/schemas.py
@@ -6,9 +6,9 @@ class Surface(pydantic.BaseModel):
     """A schema for surface objects."""
 
     name: str = pydantic.Field(..., example="Human Surface Left")
-    xCoordinate: list[int] = pydantic.Field(..., example=[1, 2, 3])
-    yCoordinate: list[int] = pydantic.Field(..., example=[1, 2, 3])
-    zCoordinate: list[int] = pydantic.Field(..., example=[1, 2, 3])
+    xCoordinate: list[float] = pydantic.Field(..., example=[1, 2, 3])
+    yCoordinate: list[float] = pydantic.Field(..., example=[1, 2, 3])
+    zCoordinate: list[float] = pydantic.Field(..., example=[1, 2, 3])
     iFaces: list[int] = pydantic.Field(..., example=[1, 2, 3])
     jFaces: list[int] = pydantic.Field(..., example=[1, 2, 3])
     kFaces: list[int] = pydantic.Field(..., example=[1, 2, 3])
