37a1a86f8f8ae8bc42db4c8f17b23222d90206dd
Reinder Vos de Wael <reinder.vosdewael@childmind.org>
2023-08-09 15:10:37 -0400

    Fix circular import

--- api/WrapperFunction/__init__.py
+++ api/WrapperFunction/__init__.py
@@ -6,17 +6,16 @@ import aiohttp
 import azure.functions as func
 import nest_asyncio
 
-from src import main as src_main
+from src import builder
 
 nest_asyncio.apply()
-
-APP = src_main.build_app()
 
 
 async def main(req: func.HttpRequest, context: func.Context) -> func.HttpResponse:
     """Each request is redirected to the ASGI handler."""
+    app = builder.build_app()
     async with aiohttp.ClientSession():
         loop = asyncio.get_running_loop()
         return await loop.run_in_executor(
-            None, func.AsgiMiddleware(APP).handle, req, context
+            None, func.AsgiMiddleware(app).handle, req, context
         )
--- api/src/main.py
+++ api/src/main.py
@@ -1,46 +1,4 @@
-"""Main module for the API."""
-import logging
+"""Entrypoint for Uvicorn."""
+from src import builder
 
-import fastapi
-from fastapi.middleware import cors
-
-from src.core import settings
-from src.routers.features import views as feature_views
-from src.routers.surfaces import views as surface_views
-
-config = settings.get_settings()
-LOGGER_NAME = config.LOGGER_NAME
-
-
-def build_app() -> fastapi.FastAPI:
-    """Builds and returns a FastAPI application instance with the necessary
-    routers and middleware.
-
-    Returns:
-        fastapi.FastAPI: The FastAPI application instance.
-    """
-    settings.initialize_logger()
-    logger = logging.getLogger(LOGGER_NAME)
-
-    api = fastapi.APIRouter(prefix="/api")
-    api.include_router(feature_views.router)
-    api.include_router(surface_views.router)
-
-    logger.info("Starting API.")
-    app = fastapi.FastAPI()
-    app.include_router(api)
-
-    logger.info("Adding CORS middleware.")
-    app.add_middleware(
-        cors.CORSMiddleware,
-        allow_origins="*",
-        allow_credentials=True,
-        allow_methods=["*"],
-        allow_headers=["*"],
-    )
-
-    return app
-
-
-if __name__ == "__main__":
-    app = build_app()
+app = builder.build_app()
