9ac0fcab74e9d5bcefaed20073f3ef8a77785569
Reinder Vos de Wael <reinder.vosdewael@gmail.com>
2023-08-09 10:30:07 -0400

    Make requests asynchronously (#20)
    * Make all requests to the API simultaneously
    
    * Async API
    
    * Add an await to the async wrapper
    
    * add nest_asyncio
    
    * co-pilot take the wheel
    
    ---------
    
    Co-authored-by: Reinder Vos de Wael <reinder.vosdewael@childmind.org>

--- api/WrapperFunction/__init__.py
+++ api/WrapperFunction/__init__.py
@@ -1,10 +1,20 @@
 """Wrapper function for Azure Functions."""
 # pylint: disable=invalid-name
+import asyncio
+
+import aiohttp
 import azure.functions as func
+import nest_asyncio
 
-from src import main as src_main
+from src.main import app
+
+nest_asyncio.apply()
 
 
-def main(req: func.HttpRequest, context: func.Context) -> func.HttpResponse:
+async def main(req: func.HttpRequest, context: func.Context) -> func.HttpResponse:
     """Each request is redirected to the ASGI handler."""
-    return func.AsgiMiddleware(src_main.app).handle(req, context)
+    async with aiohttp.ClientSession():
+        loop = asyncio.get_running_loop()
+        return await loop.run_in_executor(
+            None, func.AsgiMiddleware(app).handle, req, context
+        )
