7fbf7525e90fb50c1ed94d24e559546b29ab4cc1
Reinder Vos de Wael <reinder.vosdewael@childmind.org>
2023-08-09 09:22:36 -0400

    Async API

--- api/WrapperFunction/__init__.py
+++ api/WrapperFunction/__init__.py
@@ -1,10 +1,12 @@
 """Wrapper function for Azure Functions."""
 # pylint: disable=invalid-name
+import aiohttp
 import azure.functions as func
 
 from src import main as src_main
 
 
-def main(req: func.HttpRequest, context: func.Context) -> func.HttpResponse:
+async def main(req: func.HttpRequest, context: func.Context) -> func.HttpResponse:
     """Each request is redirected to the ASGI handler."""
-    return func.AsgiMiddleware(src_main.app).handle(req, context)
+    async with aiohttp.ClientSession():
+        return func.AsgiMiddleware(src_main.app).handle(req, context)
