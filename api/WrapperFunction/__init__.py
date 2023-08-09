ac98890a723ee72c261b13a2797cbc72bb3cf723
Reinder Vos de Wael <reinder.vosdewael@childmind.org>
2023-08-09 10:02:39 -0400

    Add an await to the async wrapper

--- api/WrapperFunction/__init__.py
+++ api/WrapperFunction/__init__.py
@@ -9,4 +9,4 @@ from src import main as src_main
 async def main(req: func.HttpRequest, context: func.Context) -> func.HttpResponse:
     """Each request is redirected to the ASGI handler."""
     async with aiohttp.ClientSession():
-        return func.AsgiMiddleware(src_main.app).handle(req, context)
+        return await func.AsgiMiddleware(src_main.app).handle(req, context)
