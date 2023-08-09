eb2a7812d8792d12007ad4ecaf65dec0771912ce
Reinder Vos de Wael <reinder.vosdewael@childmind.org>
2023-08-09 10:22:22 -0400

    co-pilot take the wheel

--- api/WrapperFunction/__init__.py
+++ api/WrapperFunction/__init__.py
@@ -1,10 +1,12 @@
 """Wrapper function for Azure Functions."""
 # pylint: disable=invalid-name
+import asyncio
+
 import aiohttp
 import azure.functions as func
 import nest_asyncio
 
-from src import main as src_main
+from src.main import app
 
 nest_asyncio.apply()
 
@@ -12,4 +14,7 @@ nest_asyncio.apply()
 async def main(req: func.HttpRequest, context: func.Context) -> func.HttpResponse:
     """Each request is redirected to the ASGI handler."""
     async with aiohttp.ClientSession():
-        return await func.AsgiMiddleware(src_main.app).handle(req, context)
+        loop = asyncio.get_running_loop()
+        return await loop.run_in_executor(
+            None, func.AsgiMiddleware(app).handle, req, context
+        )
