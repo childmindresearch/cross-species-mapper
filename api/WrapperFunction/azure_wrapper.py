f718a3480f8e161b8bccea6e6d73b84d97896772
Reinder Vos de Wael <reinder.vosdewael@childmind.org>
2023-08-28 13:41:27 -0400

    Revamp builder

--- api/WrapperFunction/azure_wrapper.py
+++ api/WrapperFunction/azure_wrapper.py
@@ -1,21 +1,5 @@
-"""Wrapper function for Azure Functions."""
-# pylint: disable=invalid-name
-import asyncio
-
-import aiohttp
 import azure.functions as func
-import nest_asyncio
 
-from src import builder
+from src.main import app as fastapi_app
 
-nest_asyncio.apply()
-
-
-async def main(req: func.HttpRequest, context: func.Context) -> func.HttpResponse:
-    """Each request is redirected to the ASGI handler."""
-    app = builder.build_app()
-    async with aiohttp.ClientSession():
-        loop = asyncio.get_running_loop()
-        return await loop.run_in_executor(
-            None, func.AsgiMiddleware(app).handle, req, context
-        )
+app = func.AsgiFunctionApp(app=fastapi_app, http_auth_level=func.AuthLevel.ANONYMOUS)
--- api/src/main.py
+++ api/src/main.py
@@ -1,4 +1,31 @@
 """Entrypoint for Uvicorn."""
-from src import builder
+import logging
 
-app = builder.build_app()
+import fastapi
+from fastapi.middleware import cors
+
+from src.core import settings
+from src.routers.features import views as feature_views
+from src.routers.surfaces import views as surface_views
+
+config = settings.get_settings()
+
+settings.initialize_logger()
+logger = logging.getLogger(config.LOGGER_NAME)
+
+api = fastapi.APIRouter(prefix="/api")
+api.include_router(feature_views.router)
+api.include_router(surface_views.router)
+
+logger.info("Starting API.")
+app = fastapi.FastAPI()
+app.include_router(api)
+
+logger.info("Adding CORS middleware.")
+app.add_middleware(
+    cors.CORSMiddleware,
+    allow_origins="*",
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
