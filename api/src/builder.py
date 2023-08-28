864f52a5324816c1f43beeea3ee15d299afdf373
Reinder Vos de Wael <reinder.vosdewael@childmind.org>
2023-08-28 13:30:22 -0400

    Attempt to fix the circular import bug (#30)
    * Cache API results
    
    * Switch to brainviewer npm
    
    * Remove nibabel
    
    * Test build app outside of async loop
    
    * Revert previous change
    
    * Remove scipy, scikit-learn
    
    * Move app build out of loop
    
    * Revert prior change, move data out of API directory
    
    * Drop chrome support
    
    * rename azure init script
    
    * rename azure init script
    
    * Import all inside the builder

--- api/src/builder.py
+++ api/src/builder.py
@@ -2,28 +2,37 @@
 
 This is separate from main.py as the app needs to be created
 inside the loop on Azure."""
-import logging
+# pylint: disable=import-outside-toplevel
 
-import fastapi
-from fastapi.middleware import cors
+import typing
 
-from src.core import settings
-from src.routers.features import views as feature_views
-from src.routers.surfaces import views as surface_views
-
-config = settings.get_settings()
-LOGGER_NAME = config.LOGGER_NAME
+if typing.TYPE_CHECKING:
+    import fastapi
 
 
-def build_app() -> fastapi.FastAPI:
+def build_app() -> "fastapi.FastAPI":
     """Builds and returns a FastAPI application instance with the necessary
     routers and middleware.
 
     Returns:
         fastapi.FastAPI: The FastAPI application instance.
+
+    Notes:
+        Imports inside the function to avoid circular imports.
     """
+    import logging
+
+    import fastapi
+    from fastapi.middleware import cors
+
+    from src.core import settings
+    from src.routers.features import views as feature_views
+    from src.routers.surfaces import views as surface_views
+
+    config = settings.get_settings()
+
     settings.initialize_logger()
-    logger = logging.getLogger(LOGGER_NAME)
+    logger = logging.getLogger(config.LOGGER_NAME)
 
     api = fastapi.APIRouter(prefix="/api")
     api.include_router(feature_views.router)
