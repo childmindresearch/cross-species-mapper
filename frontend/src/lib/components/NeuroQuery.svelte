c058ebe4ffe2399948bb00ba07b86882545579ff
Reinder Vos de Wael <reinder.vosdewael@childmind.org>
2023-10-04 16:40:32 -0400

    Download Terms

--- frontend/src/lib/components/NeuroQuery.svelte
+++ frontend/src/lib/components/NeuroQuery.svelte
@@ -1,9 +1,23 @@
 <script lang="ts">
   import { terms } from "$lib/store";
+  import Button from "./Button.svelte";
+
+  let displayTerms = $terms.slice(0, 10).join(", ");
 </script>
 
 <div class="neuroquery">
-  <i><b>Top 10 Neuroquery Terms:</b> {$terms}</i>
+  <i><b>Top 10 Neuroquery Terms:</b> {displayTerms}</i>
+  <Button
+    text="Download Terms"
+    onClick={() => {
+      const element = document.createElement("a");
+      const file = new Blob([$terms.join("\n")], { type: "text/plain" });
+      element.href = URL.createObjectURL(file);
+      element.download = "neuroquery_terms.txt";
+      document.body.appendChild(element);
+      element.click();
+    }}
+  />
 </div>
 
 <style>
