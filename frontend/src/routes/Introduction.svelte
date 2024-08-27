22e3e9068e3beb3739ac906facc9f9e7f600329f
Sam Alldritt <70346511+samalldritt@users.noreply.github.com>
2024-08-27 14:51:53 -0400

    feat: Update introduction blurb (#60)

--- frontend/src/routes/Introduction.svelte
+++ frontend/src/routes/Introduction.svelte
@@ -1,7 +1,5 @@
 <p class="mb-3">
-  The viewers below show the similarity between the human and macaque brain. To
-  rotate the brains, click and drag on any brain. To see data for a particular
-  region, double click on the region. The list of <a
+  Welcome to Interspecies Mapper! This tool is designed to facilitate translational neuroscience research across primate species. Rotating and interacting (double-clicking) with either the human or rhesus macaque brain surface maps below reveals functionally homologous regions between species. Selecting a specific region of the brain in either species will prompt the top ten terms most associated with the region based on a <a
     href="https://neuroquery.org">NeuroQuery</a
-  > terms shows the top 10 hits for the current map.
+  > search of the corresponding literature–with the option to download the top 100. The bottom figures show the growth trajectories across the associated lifespan of a selected region, based on large aggregated datasets of both human and macaque samples. Growth trajectories can be customized to show the lifespan development of gray matter volume, cortical surface area, or cortical thickness. For optimal performance, we recommend using the Chrome browser.
 </p>
